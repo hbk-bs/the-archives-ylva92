77f2961d7174af846f7eb51b3728392455430ac5
ylva <y.beischall@hbk-bs.de>
2025-01-27 14:31:42 +0100

    .

---
+++ arduino/sketch_jan6a/sketch_jan6a.ino
@@ -0,0 +1,19 @@
+// as simple sketch that shows
+// how to use loops and pulse width modulation
+
+int outpin = 9;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(outpin, OUTPUT);
+
+}
+
+void loop() {
+
+  for (int val = 0; val < 256; val++) {
+    analogWrite(outpin, val);
+    Serial.println(val);
+    delay(10);
+  }
+}
