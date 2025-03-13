54940a2d8faf0fb01a9d9123fc013d211d99cb6b
ylva <y.beischall@hbk-bs.de>
2025-03-13 14:39:26 +0100

    .

--- interaction/index.js
+++ interaction/index.js
@@ -1,24 +1,27 @@
 
 let isBlinking = false;
 let eyeHeight = 60;
-let blinkTimer = 0;
+let textSizeValue = 24;
+let textColor = [0, 0, 0]; 
 
 function setup() {
     colorMode(HSL, 360, 100, 100, 100);
-    createCanvas(600, 600);
+    createCanvas(800, 800);
     background(150, 100, 20, 30);
 }
 
 function draw() {
     background(150, 100, 20, 30);
 
-    // Augen blinzeln 
+    // Augen blinzeln lassen
     if (isBlinking) {
-        eyeHeight -= 5;  
+        eyeHeight -= 5;
         if (eyeHeight <= 5) {
-            isBlinking = false; 
+            isBlinking = false;
             setTimeout(() => {
-                eyeHeight = 60; 
+                eyeHeight = 60;
+                textSizeValue = 24; 
+                textColor = [0, 0, 0]; 
             }, 200);
         }
     }
@@ -38,19 +41,26 @@ function draw() {
     ellipse(500, 400, 80, eyeHeight);
 
     // Pupillen
-    if (eyeHeight > 10) { 
+    if (eyeHeight > 10) {
         fill(0, 0, 0, 100);
         circle(300, 400, 10);
         circle(500, 400, 10);
     }
+
+    // Text über den Augen
+    fill(textColor);
+    textAlign(CENTER, CENTER);
+    textSize(15);
+    text("Bitte nicht in die Augen pieksen!", width / 2, 150);
 }
 
 function mousePressed() {
-    
     let d1 = dist(mouseX, mouseY, 300, 400);
     let d2 = dist(mouseX, mouseY, 500, 400);
 
     if (d1 < 40 || d2 < 40) {
         isBlinking = true;
+        textColor = [0, 100, 50]; 
+        
     }
 }
